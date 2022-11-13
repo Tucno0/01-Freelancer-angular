bc2cf20bb6ec95c30c719637e29f709f4eb9c401
Tucno0 <yantucnoperez@gmail.com>
2022-11-13 15:14:52 -0500

    completado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContactoComponent } from './contacto/contacto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: InicioComponent},
+  {path: 'inicio', component: InicioComponent},
+  {path: 'sobre-mi', component: SobreMiComponent},
+  {path: 'clientes', component: ClientesComponent},
+  {path: 'contacto', component: ContactoComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavegacionComponent } from './navegacion/navegacion.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { SobreMiComponent } from './sobre-mi/sobre-mi.component';
+import { ClientesComponent } from './clientes/clientes.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { PortadaComponent } from './portada/portada.component';
+import { ServiciosComponent } from './servicios/servicios.component';
+import { FormularioComponent } from './formulario/formulario.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavegacionComponent,
+    InicioComponent,
+    SobreMiComponent,
+    ClientesComponent,
+    ContactoComponent,
+    PortadaComponent,
+    ServiciosComponent,
+    FormularioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
